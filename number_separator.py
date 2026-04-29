cbba33a75d70a90a48f440290b0a92118c3fe4dd
angeljean6 <ajrescober@gmail.com>
2026-04-29 18:48:22 +0800

    added validation logic

--- number_separator.py
+++ number_separator.py
@@ -1,3 +1,10 @@
+import os
+
 class NumberClassifier:
-    def __init__(self, source_file):
-        self.source_file = source_file
+    def __init__(self, input_file="numbers.txt"):
+        self.input_file = input_file
+
+    def split_even_odd(self):
+        if not os.path.exists(self.input_file):
+            print("Error: Input file not found.")
+            return
